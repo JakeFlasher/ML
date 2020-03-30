2a065e16508fb9a85e0504b7d3b2da1251fce1b1
JakeFlasher <jakeflasher@outlook.com>
2020-03-31 00:41:55 +0800

    isss

--- raw/utils.py
+++ raw/utils.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 import numpy as np
 from PIL import Image
 import struct 
@@ -6,7 +7,7 @@ import png
 import os
 
 # 获取指定路径下的所有 .png 文件
-def file2list(path):
+def file2list(path, prefix=".png"):
     # file_list = []
     # for filename in os.listdir(path):
     #     ele_path = os.path.join(path, filename)
@@ -15,7 +16,7 @@ def file2list(path):
     #         if (subele_path.endswith(".png")):
     #             file_list.append(subele_path)
     # return file_list
-    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".png")]
+    return [os.path.join(path, f) for f in os.listdir(path) if f.endswith(prefix)]
  
  
  
@@ -59,11 +60,11 @@ def read2convert(imgFileList):
 def read_folder_img():
     cName = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     train_data_path = "./mnist/train/0"
-    flist = file2list(train_data_path)
+    flist = file2list(train_data_path,".png")
     dataMat, dataLabel = read2convert(flist)
     for c in cName:
         train_data_path_ = "./mnist/train/" + c
-        flist_ = file2list(train_data_path_)
+        flist_ = file2list(train_data_path_,".png")
         dataMat_, dataLabel_ = read2convert(flist_)
         dataMat = np.concatenate((dataMat, dataMat_), axis=0)
         dataLabel = np.concatenate((dataLabel, dataLabel_), axis=0)
@@ -82,46 +83,76 @@ def binary2img(binfile, binlabel, folders):
                 data = [binfile[(i*28*28 + j*28) : (i*28*28 + (j+1)*28)] for j in range(28)]
                 image.write(img, data)
 
+def convert(imgf, labelf, outf, n):
+    f = open(imgf, "rb")
+    o = open(outf, "w")
+    l = open(labelf, "rb")
+
+    f.read(16)
+    l.read(8)
+    images = []
+
+    for i in range(n):
+        image = [ord(l.read(1))]
+        for j in range(28*28):
+            image.append(ord(f.read(1)))
+        images.append(image)
+
+    for image in images:
+        o.write(",".join(str(pix) for pix in image)+"\n")
+    f.close()
+    o.close()
+    l.close()
+
+def convert2csv():
+    if not os.path.exists("./mnist/fashion/fashion_test.csv"):
+        convert("./mnist/fashion/t10k-images-idx3-ubyte", "./mnist/fashion/t10k-labels-idx1-ubyte",
+                "./mnist/fashion/fashion_test.csv", 10000) 
+    if not os.path.exists("./mnist/fashion/fashion_train.csv"):
+        convert("./mnist/fashion/train-images-idx3-ubyte", "./mnist/fashion/train-labels-idx1-ubyte",
+                "./mnist/fashion/fashion_train.csv", 60000)
 
 if __name__ == "__main__":
-    trainimg = './mnist/fashion/train-images-idx3-ubyte'
-    trainlabel = './mnist/fashion/train-labels-idx1-ubyte'
-    testimg = './mnist/fashion/t10k-images-idx3-ubyte'
-    testlabel = './mnist/fashion/t10k-labels-idx1-ubyte'
-    trainfolder = './mnist/train'
-    testfolder = './mnist/test'
-    if not os.path.exists(trainfolder): os.makedirs(trainfolder)
-    if not os.path.exists(testfolder): os.makedirs(testfolder)
-    # rb表示以二进制读模式打开文件
-    trimg = open(trainimg, 'rb')
-    teimg = open(testimg, 'rb')
-    trlab = open(trainlabel, 'rb')
-    telab = open(testlabel, 'rb')
+    convert2csv()
+    if (int(intput()) == 1):
+        trainimg = './mnist/fashion/train-images-idx3-ubyte'
+        trainlabel = './mnist/fashion/train-labels-idx1-ubyte'
+        testimg = './mnist/fashion/t10k-images-idx3-ubyte'
+        testlabel = './mnist/fashion/t10k-labels-idx1-ubyte'
+        trainfolder = './mnist/train'
+        testfolder = './mnist/test'
+        if not os.path.exists(trainfolder): os.makedirs(trainfolder)
+        if not os.path.exists(testfolder): os.makedirs(testfolder)
+        # rb表示以二进制读模式打开文件
+        trimg = open(trainimg, 'rb')
+        teimg = open(testimg, 'rb')
+        trlab = open(trainlabel, 'rb')
+        telab = open(testlabel, 'rb')
 
-    struct.unpack(">IIII", trimg.read(16))
-    struct.unpack(">IIII", teimg.read(16))
-    struct.unpack(">II", trlab.read(8))
-    struct.unpack(">II", telab.read(8))
-    # array模块是Python中实现的一种高效的数组存储类型
-    # 所有数组成员都必须是同一种类型，在创建数组时就已经规定
-    # B表示无符号字节型，b表示有符号字节型
-    trimage = array("B", trimg.read())
-    teimage = array("B", teimg.read())
-    trlabel = array("b", trlab.read())
-    telabel = array("b", telab.read())
-    trimg.close()
-    teimg.close()
-    trlab.close()
-    telab.close()
-    # 为训练集和测试集各定义10个子文件夹，用于存放从0到9的所有数字，文件夹名分别为0-9
-    trainfolders = [os.path.join(trainfolder, str(i)) for i in range(10)]
-    testfolders = [os.path.join(testfolder, str(i)) for i in range(10)]
-    for dir in trainfolders:
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-    for dir in testfolders:
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-    binary2img(trimage, trlabel, trainfolders)
-    binary2img(teimage, telabel, testfolders)
-    print("Done.\n")
+        struct.unpack(">IIII", trimg.read(16))
+        struct.unpack(">IIII", teimg.read(16))
+        struct.unpack(">II", trlab.read(8))
+        struct.unpack(">II", telab.read(8))
+        # array模块是Python中实现的一种高效的数组存储类型
+        # 所有数组成员都必须是同一种类型，在创建数组时就已经规定
+        # B表示无符号字节型，b表示有符号字节型
+        trimage = array("B", trimg.read())
+        teimage = array("B", teimg.read())
+        trlabel = array("b", trlab.read())
+        telabel = array("b", telab.read())
+        trimg.close()
+        teimg.close()
+        trlab.close()
+        telab.close()
+        # 为训练集和测试集各定义10个子文件夹，用于存放从0到9的所有数字，文件夹名分别为0-9
+        trainfolders = [os.path.join(trainfolder, str(i)) for i in range(10)]
+        testfolders = [os.path.join(testfolder, str(i)) for i in range(10)]
+        for dir in trainfolders:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+        for dir in testfolders:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+        binary2img(trimage, trlabel, trainfolders)
+        binary2img(teimage, telabel, testfolders)
+        print("Done.\n")
